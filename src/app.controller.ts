a0439c54cc493aec337be6c7c1215724c92450f5
MouadKH9 <mouad.khchich@gmail.com>
2020-05-15 19:14:37 +0000

    Almost done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
+import { CreateProjectDTO } from './database/interfaces/create_project.dto';
 import { ProjectsService } from './database/services/projects.service';
 import { Project } from './database/interfaces/project';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Post, Body } from '@nestjs/common';
 
 @Controller('projects')
 export class AppController {
@@ -11,8 +12,19 @@ export class AppController {
     return await this.projectsService.findAll();
   }
 
+  @Post()
+  async createProject(@Body() createProjectDTO: CreateProjectDTO) {
+    const project = await this.projectsService.create(createProjectDTO);
+    return project;
+  }
+
   @Get(':id')
-  async getProject(@Param() params): Promise<Project> {
-    return await this.projectsService.find(params.id);
+  async getProject(@Param('id') id): Promise<Project> {
+    return await this.projectsService.find(id);
+  }
+
+  @Delete(':id')
+  async deleteProject(@Param('id') id): Promise<boolean> {
+    return await this.projectsService.delete(id);
   }
 }
